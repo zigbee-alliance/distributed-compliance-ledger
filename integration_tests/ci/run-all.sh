368e5ecd6b80f70f745b4b3cfa917b2d5558170e
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2020-09-09 12:36:12 +0300

    * Implementation

--- integration_tests/ci/run-all.sh
+++ integration_tests/ci/run-all.sh
@@ -13,50 +13,118 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 LOG_PREFIX="[run all] "
+DETAILED_OUTPUT_TARGET=/dev/null # Use /dev/stdout for debug
 
 log() {
   echo "${LOG_PREFIX}$1"
 }
 
-setup_environment() {
-  log "Setting up environment"
+wait_for_height() {
+  local target_height=${1:-1} # Default is 1
+  local wait_time=${2:-10}    # In seconds, default - 10
 
-  log "-> Compiling dcl binaries"
-  make install
+  local waited=0
+  local wait_interval=1
+
+  while true; do
+    sleep "${wait_interval}"
+    waited=$((waited + wait_interval))
+
+    current_height=$(dclcli status | grep height | sed 's/.*"\(.*\)".*/\1/')
+
+    if ((current_height >= target_height)); then
+      echo "Height $target_height is reached in $waited seconds" &>${DETAILED_OUTPUT_TARGET}
+      break
+    fi
+
+    if ((waited > wait_time)); then
+      echo "Height $target_height is not reached in $wait_time seconds" &>${DETAILED_OUTPUT_TARGET}
+      exit 1
+    fi
+
+    echo "Waiting for height: $target_height... Current height: $current_height, " \
+      "wait time: $waited, time limit: $wait_time." &>${DETAILED_OUTPUT_TARGET}
+  done
 }
 
-setup_test() {
-  log "Setting up test environment:"
+init_pool() {
+  log "Setting up pool"
 
-  log "-> Generating network configuration"
-  make localnet_init
+  log "-> Generating network configuration" >${DETAILED_OUTPUT_TARGET}
+  make localnet_init &>${DETAILED_OUTPUT_TARGET}
 
-  log "-> Running pool"
-  make localnet_start
+  log "-> Running pool" >${DETAILED_OUTPUT_TARGET}
+  make localnet_start &>${DETAILED_OUTPUT_TARGET}
+
+  log "-> Waiting for the second block (needed to request proofs)" >${DETAILED_OUTPUT_TARGET}
+  wait_for_height 2 20
 }
 
-cleanup_test() {
-  log "Cleaning up test:"
+cleanup_pool() {
+  log "Cleaning up pool"
 
-  log "-> Removing configurations"
+  log "-> Stopping pool" >${DETAILED_OUTPUT_TARGET}
+  make localnet_stop &>${DETAILED_OUTPUT_TARGET}
+
+  log "-> Removing configurations" >${DETAILED_OUTPUT_TARGET}
   rm -rf ~/.dclcli
   rm -rf ~/.dcld
-  rm -rf localnet
+  sudo rm -rf localnet
 }
 
+run_rest_server() {
+  log "Running cli in rest-server mode"
+  dclcli rest-server --chain-id dclchain &>>rest-server.out &
+}
 
-setup_environment
-cleanup_test
+stop_rest_server() {
+  log "Stopping cli in rest-server mode"
+  killall dclcli
+}
 
-SHELL_TESTS=$(find integration_tests/cli -type f -not -name "common.sh")
+# Global init
+set -e
+make install &>${DETAILED_OUTPUT_TARGET}
 
-for SHELL_TEST in ${SHELL_TESTS}; do
-  setup_test
+# Cli shell tests
+CLI_SHELL_TESTS=$(find integration_tests/cli -type f -not -name "common.sh")
 
-  log "Running $SHELL_TEST"
-  sh "$SHELL_TEST"
+for CLI_SHELL_TEST in ${CLI_SHELL_TESTS}; do
+  init_pool
 
-  cleanup_test
+  log "Running $CLI_SHELL_TEST"
+
+  if bash "$CLI_SHELL_TEST" &>"test.out"; then
+    cat "test.out" >${DETAILED_OUTPUT_TARGET}
+    log "$CLI_SHELL_TEST finished successfully"
+  else
+    cat "test.out"
+    log "$CLI_SHELL_TEST falied"
+    exit 1
+  fi
+
+  cleanup_pool
+done
+
+# Go rest tests
+GO_REST_TESTS=$(find integration_tests/rest -type f)
+
+for GO_REST_TEST in ${GO_REST_TESTS}; do
+  init_pool
+  run_rest_server
+
+  log "Running $GO_REST_TEST"
+
+  if go test "$GO_REST_TEST" &>"test.out"; then
+    cat "test.out" >${DETAILED_OUTPUT_TARGET}
+    log "$GO_REST_TEST finished successfully"
+  else
+    cat "test.out"
+    log "$GO_REST_TEST falied"
+    exit 1
+  fi
+
+  stop_rest_server
+  cleanup_pool
 done
