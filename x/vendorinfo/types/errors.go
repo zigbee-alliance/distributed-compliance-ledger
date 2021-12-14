6b56bf920bd008e9fba472defdc4c2ccaaccefb0
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2021-12-14 15:23:18 +0300

    migrated errors

--- x/vendorinfo/types/errors.go
+++ x/vendorinfo/types/errors.go
@@ -3,10 +3,31 @@ package types
 // DONTCOVER
 
 import (
+	"fmt"
+
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // x/vendorinfo module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	DefaultCodespace string = ModuleName
+
+	CodeVendorDoesNotExist              uint32 = 701
+	CodeMissingVendorIDForVendorAccount uint32 = 702
+	CodeVendorInfoAlreadyExists         uint32 = 703
 )
+
+func ErrVendorInfoDoesNotExist(vendorID uint16) *sdkerrors.Error {
+	return sdkerrors.Register(DefaultCodespace, CodeVendorDoesNotExist,
+		fmt.Sprintf("Vendor Account with VendorID %v does not exist on the ledger", vendorID))
+}
+
+func ErrMissingVendorIDForVendorAccount() *sdkerrors.Error {
+	return sdkerrors.Register(DefaultCodespace, CodeMissingVendorIDForVendorAccount,
+		"No Vendor ID is provided in the Vendor Role for the new account")
+}
+
+func ErrVendorInfoAlreadyExists(vendorID interface{}) *sdkerrors.Error {
+	return sdkerrors.Register(DefaultCodespace, CodeVendorInfoAlreadyExists,
+		fmt.Sprintf("Vendor info associated with VendorID=%v already exists on the ledger", vendorID))
+}
