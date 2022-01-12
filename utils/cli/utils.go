9c7acee2e1d83d29f490182f7dc539428294d6f4
Andrey Kononykhin <andkononykhin@gmail.com>
2022-01-12 12:50:32 +0300

    disables dry-run option for txs
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- utils/cli/utils.go
+++ utils/cli/utils.go
@@ -50,11 +50,9 @@ func AddTxFlagsToCmd(cmd *cobra.Command) {
 		flags.FlagGasAdjustment,
 		flags.FlagGas,
 		flags.FlagFeeAccount,
+		flags.FlagDryRun, // TODO that flag might be actually useful but relates to gas
 	}
 	for _, f := range hiddenFlags {
 		cmd.Flags().MarkHidden(f)
 	}
-
-	// TODO is it possible to update a usage for already added flag ? (the below option will fail)
-	// cmd.Flags().Bool(flags.FlagDryRun, false, "perform a simulation of a transaction, but don't broadcast it")
 }
