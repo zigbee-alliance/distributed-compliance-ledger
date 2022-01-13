f7a12dd2fa03d04c1f61ee253cfa2379295defdb
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2022-01-13 18:53:40 +0300

    Slash/Jail Byzantine nodes (that is remove from the validator set)

--- x/validator/module.go
+++ x/validator/module.go
@@ -158,8 +158,7 @@ func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the validator module.
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
-	// enable once something valuable appears here
-	// BeginBlocker(ctx, am.keeper)
+	BeginBlocker(ctx, req, am.keeper)
 }
 
 // EndBlock executes all ABCI EndBlock logic respective to the validator module.
