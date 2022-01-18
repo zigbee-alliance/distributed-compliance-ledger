c286d6f1e1fd717d8c1303490a442b6c6143552d
Denis Rybas <denis2002.rybas@gmail.com>
2022-01-18 10:01:57 +0300

    pki tests added

--- x/pki/types/message_approve_add_x_509_root_cert_test.go
+++ x/pki/types/message_approve_add_x_509_root_cert_test.go
@@ -1,16 +1,17 @@
 package types
 
-/* TODO issue 99
 import (
 	"testing"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
+	testconstants "github.com/zigbee-alliance/distributed-compliance-ledger/integration_tests/constants"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/testutil/sample"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/utils/validator"
 )
 
 func TestMsgApproveAddX509RootCert_ValidateBasic(t *testing.T) {
-	tests := []struct {
+	negative_tests := []struct {
 		name string
 		msg  MsgApproveAddX509RootCert
 		err  error
@@ -18,25 +19,58 @@ func TestMsgApproveAddX509RootCert_ValidateBasic(t *testing.T) {
 		{
 			name: "invalid address",
 			msg: MsgApproveAddX509RootCert{
-				Signer: "invalid_address",
+				Signer:       "invalid_address",
+				Subject:      testconstants.RootSubject,
+				SubjectKeyId: testconstants.RootSubjectKeyID,
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+		},
+		{
+			name: "empty subject",
 			msg: MsgApproveAddX509RootCert{
-				Signer: sample.AccAddress(),
+				Signer:       sample.AccAddress(),
+				Subject:      "",
+				SubjectKeyId: testconstants.RootSubjectKeyID,
+			},
+			err: validator.ErrRequiredFieldMissing,
+		},
+		{
+			name: "empty SubjectKeyId",
+			msg: MsgApproveAddX509RootCert{
+				Signer:       sample.AccAddress(),
+				Subject:      testconstants.RootSubject,
+				SubjectKeyId: "",
+			},
+			err: validator.ErrRequiredFieldMissing,
+		},
+	}
+
+	positive_tests := []struct {
+		name string
+		msg  MsgApproveAddX509RootCert
+	}{
+		{
+			name: "valid approve add x509cert msg",
+			msg: MsgApproveAddX509RootCert{
+				Signer:       sample.AccAddress(),
+				Subject:      testconstants.RootSubject,
+				SubjectKeyId: testconstants.RootSubjectKeyID,
 			},
 		},
 	}
-	for _, tt := range tests {
+
+	for _, tt := range negative_tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
-			}
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
+		})
+	}
+
+	for _, tt := range positive_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
 			require.NoError(t, err)
 		})
 	}
 }
-*/
