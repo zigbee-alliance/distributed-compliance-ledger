d3a270a1bb3235e98fc50e409ad8954afc9f8377
Nikita Spivachuk <nikita.spivachuk@dsr-corporation.com>
2022-01-27 19:13:09 +0000

    Added address restrictions to MsgProposeUpgrade and ProposedUpgrade types

--- x/dclupgrade/types/message_propose_upgrade.go
+++ x/dclupgrade/types/message_propose_upgrade.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/utils/validator"
 )
 
 const TypeMsgProposeUpgrade = "propose_upgrade"
@@ -41,6 +42,11 @@ func (msg *MsgProposeUpgrade) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+
+	err = validator.Validate(msg)
+	if err != nil {
+		return err
 	}
 
 	err = msg.Plan.ValidateBasic()
