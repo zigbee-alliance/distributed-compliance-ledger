0a7b5c4b3d527dbf15c52479385d58ce2d1da3d0
Nikita Spivachuk <nikita.spivachuk@dsr-corporation.com>
2022-01-31 16:24:05 +0000

    Added address restriction and validation to MsgApproveUpgrade type

--- x/dclupgrade/types/message_approve_upgrade.go
+++ x/dclupgrade/types/message_approve_upgrade.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/utils/validator"
 )
 
 const TypeMsgApproveUpgrade = "approve_upgrade"
@@ -42,5 +43,11 @@ func (msg *MsgApproveUpgrade) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+
+	err = validator.Validate(msg)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
