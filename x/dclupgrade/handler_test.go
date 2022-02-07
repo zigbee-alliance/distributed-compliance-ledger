8d31cfa8a2791310d93c9ac07c970d8b0176c608
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-02-07 17:07:37 +0300

    linter ignore added

--- x/dclupgrade/handler_test.go
+++ x/dclupgrade/handler_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:testpackage,lll
+//nolint:testpackage,lll, staticcheck
 package dclupgrade
 
 import (
