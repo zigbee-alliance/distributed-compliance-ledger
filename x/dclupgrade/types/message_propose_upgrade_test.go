f9612fbee77968641c81122fe1d35dbc5c6c7b49
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-02-09 12:01:37 +0300

    upgrade message tests added

--- x/dclupgrade/types/message_propose_upgrade_test.go
+++ x/dclupgrade/types/message_propose_upgrade_test.go
@@ -10,7 +10,37 @@ import (
 )
 
 func TestMsgProposeUpgrade_ValidateBasic(t *testing.T) {
-	tests := []struct {
+	// negative test constants
+
+	planNameLen0 := Plan{
+		Name:   "",
+		Height: 1,
+		Info:   "Some info",
+	}
+
+	planHeight0 := Plan{
+		Name:   "Some plan name",
+		Height: 0,
+		Info:   "Some info",
+	}
+
+	planHeightLess0 := Plan{
+		Name:   "Some plan name",
+		Height: -1,
+		Info:   "Some info",
+	}
+
+	// positive test constants
+
+	planNormal := testconstants.Plan
+
+	planInfoLen0 := Plan{
+		Name:   "Some plan name",
+		Height: 1,
+		Info:   "",
+	}
+
+	negative_tests := []struct {
 		name string
 		msg  MsgProposeUpgrade
 		err  error
@@ -22,22 +52,64 @@ func TestMsgProposeUpgrade_ValidateBasic(t *testing.T) {
 				Plan:    testconstants.Plan,
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+		},
+		{
+			name: "Plan len 0",
 			msg: MsgProposeUpgrade{
 				Creator: sample.AccAddress(),
-				Plan:    testconstants.Plan,
+				Plan:    planNameLen0,
+			},
+			err: sdkerrors.ErrInvalidRequest,
+		},
+		{
+			name: "Plan height 0",
+			msg: MsgProposeUpgrade{
+				Creator: sample.AccAddress(),
+				Plan:    planHeight0,
+			},
+			err: sdkerrors.ErrInvalidRequest,
+		},
+		{
+			name: "Plan height less than 0",
+			msg: MsgProposeUpgrade{
+				Creator: sample.AccAddress(),
+				Plan:    planHeightLess0,
+			},
+			err: sdkerrors.ErrInvalidRequest,
+		},
+	}
+
+	positive_tests := []struct {
+		name string
+		msg  MsgProposeUpgrade
+	}{
+		{
+			name: "valid msg propose upgrade message",
+			msg: MsgProposeUpgrade{
+				Creator: sample.AccAddress(),
+				Plan:    planNormal,
+			},
+		},
+		{
+			name: "info len 0",
+			msg: MsgProposeUpgrade{
+				Creator: sample.AccAddress(),
+				Plan:    planInfoLen0,
 			},
 		},
 	}
-	for _, tt := range tests {
+	for _, tt := range positive_tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
-			}
 			require.NoError(t, err)
+		})
+	}
+
+	for _, tt := range negative_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
 		})
 	}
 }
