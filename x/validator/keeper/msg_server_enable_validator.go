92d1232e7666aa553ca531e69d6709c2bf8f03ee
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-03-04 00:42:38 +0300

    msg_server updated

--- x/validator/keeper/msg_server_enable_validator.go
+++ x/validator/keeper/msg_server_enable_validator.go
@@ -4,14 +4,35 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	dclauthtypes "github.com/zigbee-alliance/distributed-compliance-ledger/x/dclauth/types"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/x/validator/types"
 )
 
 func (k msgServer) EnableValidator(goCtx context.Context, msg *types.MsgEnableValidator) (*types.MsgEnableValidatorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	creatorAddr, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid Address: (%s)", err)
+	}
+
+	// check if message creator has enough rights to propose disable validator
+	if !k.dclauthKeeper.HasRole(ctx, creatorAddr, dclauthtypes.NodeAdmin) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized,
+			"Enable validator transaction should be signed by an account with the %s role",
+			dclauthtypes.NodeAdmin,
+		)
+	}
+
+	// check if disabled validator exists
+	_, isFound := k.GetDisabledValidator(ctx, msg.Address)
+	if isFound {
+		return nil, types.NewErrProposedDisableValidatorAlreadyExists(msg.Address)
+	}
+
+	// store disabled validator
+	k.RemoveDisabledValidator(ctx, msg.Address)
 
 	return &types.MsgEnableValidatorResponse{}, nil
 }
