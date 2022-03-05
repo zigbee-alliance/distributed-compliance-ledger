9b76ac72c36a080ccd504c6929367d2a38c68d79
Denis Rybas <61991100+DenisRybas@users.noreply.github.com>
2022-03-05 16:51:39 +0300

    tests updated

--- x/validator/types/message_approve_disable_validator_test.go
+++ x/validator/types/message_approve_disable_validator_test.go
@@ -5,36 +5,74 @@ import (
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
+	testconstants "github.com/zigbee-alliance/distributed-compliance-ledger/integration_tests/constants"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/testutil/sample"
 )
 
 func TestMsgApproveDisableValidator_ValidateBasic(t *testing.T) {
-	tests := []struct {
+	negative_tests := []struct {
 		name string
 		msg  MsgApproveDisableValidator
 		err  error
 	}{
 		{
-			name: "invalid address",
+			name: "invalid creator address",
 			msg: MsgApproveDisableValidator{
 				Creator: "invalid_address",
+				Address: testconstants.ValidatorAddress1,
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+		},
+		{
+			name: "omitted creator address",
+			msg: MsgApproveDisableValidator{
+				Creator: "",
+				Address: testconstants.ValidatorAddress1,
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid validator address",
+			msg: MsgApproveDisableValidator{
+				Creator: testconstants.Address1.String(),
+				Address: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "omitted validator address",
+			msg: MsgApproveDisableValidator{
+				Creator: testconstants.Address1.String(),
+				Address: "",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+	}
+
+	positive_tests := []struct {
+		name string
+		msg  MsgApproveDisableValidator
+	}{
+		{
+			name: "valid ApproveDisableValidator message",
 			msg: MsgApproveDisableValidator{
 				Creator: sample.AccAddress(),
+				Address: testconstants.ValidatorAddress1,
 			},
 		},
 	}
-	for _, tt := range tests {
+	for _, tt := range positive_tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
-			}
 			require.NoError(t, err)
+		})
+	}
+
+	for _, tt := range negative_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
 		})
 	}
 }
--- x/validator/types/message_disable_validator_test.go
+++ x/validator/types/message_disable_validator_test.go
@@ -5,36 +5,74 @@ import (
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
+	testconstants "github.com/zigbee-alliance/distributed-compliance-ledger/integration_tests/constants"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/testutil/sample"
 )
 
 func TestMsgDisableValidator_ValidateBasic(t *testing.T) {
-	tests := []struct {
+	negative_tests := []struct {
 		name string
 		msg  MsgDisableValidator
 		err  error
 	}{
 		{
-			name: "invalid address",
+			name: "invalid creator address",
 			msg: MsgDisableValidator{
 				Creator: "invalid_address",
+				Address: testconstants.ValidatorAddress1,
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+		},
+		{
+			name: "omitted creator address",
+			msg: MsgDisableValidator{
+				Creator: "",
+				Address: testconstants.ValidatorAddress1,
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid validator address",
+			msg: MsgDisableValidator{
+				Creator: testconstants.Address1.String(),
+				Address: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "omitted validator address",
+			msg: MsgDisableValidator{
+				Creator: testconstants.Address1.String(),
+				Address: "",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+	}
+
+	positive_tests := []struct {
+		name string
+		msg  MsgDisableValidator
+	}{
+		{
+			name: "valid DisableValidator message",
 			msg: MsgDisableValidator{
 				Creator: sample.AccAddress(),
+				Address: testconstants.ValidatorAddress1,
 			},
 		},
 	}
-	for _, tt := range tests {
+	for _, tt := range positive_tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
-			}
 			require.NoError(t, err)
+		})
+	}
+
+	for _, tt := range negative_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
 		})
 	}
 }
