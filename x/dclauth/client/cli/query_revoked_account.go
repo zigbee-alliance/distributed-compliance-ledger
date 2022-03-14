d6eee79c4152d377ab388b48642b875c0b1dbddd
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-03-14 21:09:44 +0500

    Add query for getting specific revoked accounts and all revoked accounts.

---
+++ x/dclauth/client/cli/query_revoked_account.go
@@ -0,0 +1,73 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/x/dclauth/types"
+)
+
+func CmdListRevokedAccount() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-revoked-account",
+		Short: "list all RevokedAccount",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllRevokedAccountRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.RevokedAccountAll(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowRevokedAccount() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-revoked-account [address]",
+		Short: "shows a RevokedAccount",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			argAddress := args[0]
+
+			params := &types.QueryGetRevokedAccountRequest{
+				Address: argAddress,
+			}
+
+			res, err := queryClient.RevokedAccount(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/dclauth/keeper/grpc_query_revoked_account.go
@@ -0,0 +1,57 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/x/dclauth/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) RevokedAccountAll(c context.Context, req *types.QueryAllRevokedAccountRequest) (*types.QueryAllRevokedAccountResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var revokedAccounts []types.RevokedAccount
+	ctx := sdk.UnwrapSDKContext(c)
+
+	store := ctx.KVStore(k.storeKey)
+	revokedAccountStore := prefix.NewStore(store, types.KeyPrefix(types.RevokedAccountKeyPrefix))
+
+	pageRes, err := query.Paginate(revokedAccountStore, req.Pagination, func(key []byte, value []byte) error {
+		var revokedAccount types.RevokedAccount
+		if err := k.cdc.Unmarshal(value, &revokedAccount); err != nil {
+			return err
+		}
+
+		revokedAccounts = append(revokedAccounts, revokedAccount)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllRevokedAccountResponse{RevokedAccount: revokedAccounts, Pagination: pageRes}, nil
+}
+
+func (k Keeper) RevokedAccount(c context.Context, req *types.QueryGetRevokedAccountRequest) (*types.QueryGetRevokedAccountResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.NotFound, "not found")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	val, found := k.GetRevokedAccount(
+		ctx,
+		req.Address,
+	)
+	if !found {
+		return nil, status.Error(codes.NotFound, "not found")
+	}
+
+	return &types.QueryGetRevokedAccountResponse{RevokedAccount: val}, nil
+}
