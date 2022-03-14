48f6589d7a9fc4a17f7fd90d5e7923032738beea
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-03-14 21:12:24 +0500

    Added implementation for saving map RevokedAccount

--- x/dclauth/keeper/msg_server_approve_revoke_account.go
+++ x/dclauth/keeper/msg_server_approve_revoke_account.go
@@ -55,6 +55,18 @@ func (k msgServer) ApproveRevokeAccount(goCtx context.Context, msg *types.MsgApp
 
 	// check if pending account revocation has enough approvals
 	if len(revoc.Approvals) == k.AccountApprovalsCount(ctx) {
+		// get account
+		account, ok := k.GetAccountO(ctx, sdk.AccAddress(accAddr))
+
+		// check we can get that account or can not
+		if !ok {
+			return nil, types.ErrAccountDoesNotExist(msg.Address)
+		}
+
+		// save to map RevokedAccount
+		revokedAccount := types.NewRevokedAccount(&account, revoc.Approvals, msg.Info, msg.Time)
+		k.SetRevokedAccount(ctx, *revokedAccount)
+
 		// delete account record
 		k.RemoveAccount(ctx, accAddr)
 
