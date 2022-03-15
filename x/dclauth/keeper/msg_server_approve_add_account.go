61a22381d279ed28aaded31783eebde70c3c46c9
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-03-15 15:10:00 +0500

    Added implementation for revoke from map RevokedAccount

--- x/dclauth/keeper/msg_server_approve_add_account.go
+++ x/dclauth/keeper/msg_server_approve_add_account.go
@@ -69,6 +69,9 @@ func (k msgServer) ApproveAddAccount(
 		}
 		k.SetAccountO(ctx, *account)
 
+		// delete from map RevokedAccount
+		k.RemoveRevokedAccount(ctx, accAddr)
+
 		// delete pending account record
 		k.RemovePendingAccount(ctx, accAddr)
 	} else {
