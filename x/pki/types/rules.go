1e46a88b6f0d0cbf1bd10e948a08a66c5399fab6
Ulfat <ulfat.abjabborov@dsr-corporation.com>
2022-03-31 10:47:45 +0500

    Remove redundant type declaration

--- x/pki/types/rules.go
+++ x/pki/types/rules.go
@@ -4,6 +4,6 @@ import dclauthtypes "github.com/zigbee-alliance/distributed-compliance-ledger/x/
 
 // TODO: 1. Move it to separate module  	2. Make it configurable		3. Save into store.
 var (
-	RootCertificateApprovalsPercent float64 = 0.66
-	RootCertificateApprovalRole             = dclauthtypes.Trustee
+	RootCertificateApprovalsPercent = 0.66
+	RootCertificateApprovalRole     = dclauthtypes.Trustee
 )
