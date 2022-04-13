d532799a9842d62a9537fb53c0e9539014b76930
Ulfat <ulfat.abjabborov@dsr-corporation.com>
2022-04-13 17:49:25 +0500

    Add vpc_network_prefix local for private sentries VPC

--- deployment/terraform/aws/private-sentries/vpc.tf
+++ deployment/terraform/aws/private-sentries/vpc.tf
@@ -2,17 +2,20 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals {
+    vpc_network_prefix = "10.10"
+}
+
 module "this_vpc" {
     source = "terraform-aws-modules/vpc/aws"
     version = "3.14.0"
 
     name = "private-sentries-vpc"
-    cidr = "10.1.0.0/16"
+    cidr = "${local.vpc_network_prefix}.0.0/16"
 
     azs                = [data.aws_availability_zones.available.names[0]]
 
-    # private_subnets    = ["10.1.1.0/24"]
-    public_subnets     = ["10.1.1.0/24"]
+    public_subnets     = ["${local.vpc_network_prefix}.1.0/24"]
 
     enable_nat_gateway = true
     enable_dns_hostnames = true
