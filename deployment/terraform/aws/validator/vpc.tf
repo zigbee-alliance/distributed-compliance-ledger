b299621d363963cbba65ddcd86667df7dca2fb9f
Ulfat <ulfat.abjabborov@dsr-corporation.com>
2022-04-13 17:49:11 +0500

    Add vpc_network_prefix local for validator VPC

--- deployment/terraform/aws/validator/vpc.tf
+++ deployment/terraform/aws/validator/vpc.tf
@@ -1,17 +1,21 @@
 data "aws_availability_zones" "available" {
     state = "available"
 }
+
+locals {
+    vpc_network_prefix = "10.0"
+}
+
 module "this_vpc" {
     source = "terraform-aws-modules/vpc/aws"
     version = "3.14.0"
 
     name = "validator-vpc"
-    cidr = "10.0.0.0/16"
+    cidr = "${local.vpc_network_prefix}.0.0/16"
 
     azs                = [data.aws_availability_zones.available.names[0]]
 
-    # private_subnets    = ["10.0.1.0/24"]
-    public_subnets     = ["10.0.1.0/24"]
+    public_subnets     = ["${local.vpc_network_prefix}.1.0/24"]
 
     enable_nat_gateway = true
     enable_dns_hostnames = true
