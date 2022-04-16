4fb3dd6e1121e004a1f70d1f88e979e108c925a4
abdulla-ashurov <abdulla-ashurov@dsr-corporation.com>
2022-04-16 19:23:24 +0500

    Correct golangci-lint mistakes

--- x/validator/handler.go
+++ x/validator/handler.go
@@ -39,6 +39,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgRejectDisableNode:
 			res, err := msgServer.RejectDisableNode(sdk.WrapSDKContext(ctx), msg)
+
 			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		default:
