bff281a9d9df1b04ff0c5726df028cfc0999e7d1
Ulfat <ulfat.abjabborov@dsr-corporation.com>
2022-04-21 18:30:50 +0500

    Generate terraform output for ansible inventory

---
+++ deployment/terraform/aws/locals.tf
@@ -0,0 +1,35 @@
+locals {
+    nodes = {
+        validator = {
+            private_ips = module.validator.private_ips
+            public_ips = module.validator.public_ips
+        },
+
+        private_sentries = {
+            private_ips = var.private_sentries_config.enable ? module.private_sentries[0].private_ips : []
+            public_ips  = var.private_sentries_config.enable ? module.private_sentries[0].public_ips : []
+            public_eips = var.private_sentries_config.enable ? module.private_sentries[0].public_eips : []
+        },
+
+        public_sentries = {
+            public_ips  = concat(
+                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 1)) ? module.public_sentries_1[0].public_ips : [], 
+                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 2)) ? module.public_sentries_2[0].public_ips : [],
+                                )
+        },
+
+        seeds = {
+            public_ips  = concat(
+                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 1)) ? module.public_sentries_1[0].seed_public_ips : [], 
+                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 2)) ? module.public_sentries_2[0].seed_public_ips : [],
+                                )
+        },
+
+        observers = {
+            public_ips  = concat(
+                                    (var.private_sentries_config.enable && var.observers_config.enable && contains(var.observers_config.regions, 1)) ? module.observers_1[0].public_ips : [], 
+                                    (var.private_sentries_config.enable && var.observers_config.enable && contains(var.observers_config.regions, 2)) ? module.observers_2[0].public_ips : [],
+                                )
+        },
+    }
+}
--- deployment/terraform/aws/outputs.tf
+++ deployment/terraform/aws/outputs.tf
@@ -1,35 +1,31 @@
 output "nodes" {
-    value = {
-        validator = {
-            private_ips = module.validator.private_ips
-            public_ips = module.validator.public_ips
-        },
+    value = local.nodes
+}
 
-        private_sentries = {
-            private_ips = var.private_sentries_config.enable ? module.private_sentries[0].private_ips : []
-            public_ips  = var.private_sentries_config.enable ? module.private_sentries[0].public_ips : []
-            public_eips = var.private_sentries_config.enable ? module.private_sentries[0].public_eips : []
-        },
+output "ansible_inventory" {
+    value = { 
+        all = {
+            children = {
+                validators = {
+                    hosts = { for host in local.nodes.validator.public_ips: host => null } 
+                }
 
-        public_sentries = {
-            public_ips  = concat(
-                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 1)) ? module.public_sentries_1[0].public_ips : [], 
-                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 2)) ? module.public_sentries_2[0].public_ips : [],
-                                )
-        },
+                private_sentries = {
+                    hosts = { for host in local.nodes.private_sentries.public_ips: host => null } 
+                }
 
-        seeds = {
-            public_ips  = concat(
-                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 1)) ? module.public_sentries_1[0].seed_public_ips : [], 
-                                    (var.private_sentries_config.enable && var.public_sentries_config.enable && contains(var.public_sentries_config.regions, 2)) ? module.public_sentries_2[0].seed_public_ips : [],
-                                )
-        },
+                public_sentries = {
+                    hosts = { for host in local.nodes.public_sentries.public_ips: host => null }  
+                }
 
-        observers = {
-            public_ips  = concat(
-                                    (var.private_sentries_config.enable && var.observers_config.enable && contains(var.observers_config.regions, 1)) ? module.observers_1[0].public_ips : [], 
-                                    (var.private_sentries_config.enable && var.observers_config.enable && contains(var.observers_config.regions, 2)) ? module.observers_2[0].public_ips : [],
-                                )
-        },
+                seeds = { 
+                    hosts = { for host in local.nodes.seeds.public_ips: host => null }
+                }
+
+                observers = {
+                    hosts = { for host in local.nodes.observers.public_ips: host => null }
+                }
+            }
+        }
     }
 }
