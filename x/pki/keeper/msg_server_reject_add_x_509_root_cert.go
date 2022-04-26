a5c358fd9064c2337a95fb5ac589fd80bff51bf6
abdulla-ashurov <abdulla-ashurov@dsr-corporation.com>
2022-04-26 15:30:53 +0500

    Add implementation for reject-add-x509-root-cert

--- x/pki/keeper/msg_server_reject_add_x_509_root_cert.go
+++ x/pki/keeper/msg_server_reject_add_x_509_root_cert.go
@@ -4,14 +4,83 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/x/pki/types"
 )
 
 func (k msgServer) RejectAddX509RootCert(goCtx context.Context, msg *types.MsgRejectAddX509RootCert) (*types.MsgRejectAddX509RootCertResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	signerAddr, err := sdk.AccAddressFromBech32(msg.Signer)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid Address: (%s)", err)
+	}
+
+	// check if signer has root certificate approval role
+	if !k.dclauthKeeper.HasRole(ctx, signerAddr, types.RootCertificateApprovalRole) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized,
+			"MsgApproveAddX509RootCert transaction should be signed by an account with the \"%s\" role",
+			types.RootCertificateApprovalRole,
+		)
+	}
+
+	// get proposed certificate
+	proposedCertificate, found := k.GetProposedCertificate(ctx, msg.Subject, msg.SubjectKeyId)
+	if !found {
+		return nil, types.NewErrProposedCertificateDoesNotExist(msg.Subject, msg.SubjectKeyId)
+	}
+
+	// check if proposed certificate already has reject approval form signer
+	if proposedCertificate.HasRejectApprovalFrom(signerAddr.String()) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized,
+			"Certificate associated with subject=%v and subjectKeyID=%v combination "+
+				"already has reject approval from=%v",
+			msg.Subject, msg.SubjectKeyId, msg.Signer,
+		)
+	}
+
+	// check if proposed certificate already has approval form signer
+	if proposedCertificate.HasApprovalFrom(signerAddr.String()) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized,
+			"Certificate associated with subject=%v and subjectKeyID=%v combination "+
+				"already has approval from=%v",
+			msg.Subject, msg.SubjectKeyId, msg.Signer,
+		)
+	}
+
+	// append approval
+	grant := types.Grant{
+		Address: signerAddr.String(),
+		Time:    msg.Time,
+		Info:    msg.Info,
+	}
+	proposedCertificate.RejectApprovals = append(proposedCertificate.RejectApprovals, &grant)
+
+	// check if proposed certificate has enough approvals
+	if len(proposedCertificate.RejectApprovals) == k.CertificateRejectApprovalsCount(ctx, k.dclauthKeeper) {
+		// create rejected certificate
+		rejectedRootCertificate := types.RejectedCertificate{
+			Subject:      proposedCertificate.Subject,
+			SubjectKeyId: proposedCertificate.SubjectKeyId,
+			Certs: []*types.Certificate{
+				{
+					PemCert:         proposedCertificate.PemCert,
+					SerialNumber:    proposedCertificate.SerialNumber,
+					Owner:           proposedCertificate.Owner,
+					Subject:         proposedCertificate.Subject,
+					SubjectAsText:   proposedCertificate.SubjectAsText,
+					SubjectKeyId:    proposedCertificate.SubjectKeyId,
+					Approvals:       proposedCertificate.Approvals,
+					RejectApprovals: proposedCertificate.RejectApprovals,
+				},
+			},
+		}
+
+		k.SetRejectedCertificate(ctx, rejectedRootCertificate)
+	} else {
+		// update proposed certificate
+		k.SetProposedCertificate(ctx, proposedCertificate)
+	}
 
 	return &types.MsgRejectAddX509RootCertResponse{}, nil
 }
