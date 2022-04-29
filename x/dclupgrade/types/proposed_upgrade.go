1be067ae18f7bcd7be30856dd1149d58d1cea8bb
abdulla-ashurov <abdulla-ashurov@dsr-corporation.com>
2022-04-29 09:48:27 +0500

    Add function for checking has reject from trustee

--- x/dclupgrade/types/proposed_upgrade.go
+++ x/dclupgrade/types/proposed_upgrade.go
@@ -12,3 +12,14 @@ func (upgrade ProposedUpgrade) HasApprovalFrom(address sdk.AccAddress) bool {
 
 	return false
 }
+
+func (upgrade ProposedUpgrade) HasRejectFrom(address sdk.AccAddress) bool {
+	addrStr := address.String()
+	for _, reject := range upgrade.Rejects {
+		if reject.Address == addrStr {
+			return true
+		}
+	}
+
+	return false
+}
