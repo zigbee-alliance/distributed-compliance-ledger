6a75db9649ef1bcdbe967e6460115ce459a30309
abdulla-ashurov <abdulla-ashurov@dsr-corporation.com>
2022-05-04 16:39:22 +0500

    Add CLI integration tests for dclupgrade

--- integration_tests/cli/upgrade-demo.sh
+++ integration_tests/cli/upgrade-demo.sh
@@ -16,6 +16,70 @@
 set -euo pipefail
 source integration_tests/cli/common.sh
 
+# get_height current_height
+# echo "Current height is $current_height"
+
+# plan_height=$(expr $current_height + 2)
+# random_string upgrade_info
+# random_string upgrade_name
+
+# echo "approve upgrade plan height less block height and we can re-propose upgrade and approve"
+
+# test_divider
+
+# propose=$(dcld tx dclupgrade propose-upgrade --name=$upgrade_name --upgrade-height=$plan_height --upgrade-info=$upgrade_info --from jack --yes)
+# echo "propose upgrade response: $propose"
+# check_response "$propose" "\"code\": 0"
+
+# test_divider
+
+# proposed_dclupgrade_query=$(dcld query dclupgrade proposed-upgrade --name=$upgrade_name)
+# echo "dclupgrade proposed upgrade query: $proposed_dclupgrade_query"
+# check_response_and_report "$proposed_dclupgrade_query" "\"name\": \"$upgrade_name\""
+# check_response_and_report "$proposed_dclupgrade_query" "\"height\": \"$plan_height\""
+# check_response_and_report "$proposed_dclupgrade_query" "\"info\": \"$upgrade_info\""
+
+# test_divider
+
+# sleep 25
+
+# approve=$(dcld tx dclupgrade approve-upgrade --name=$upgrade_name --from alice --yes 2>&1 || true)
+# check_response_and_report "$approve" "\"code\": 0"
+
+# test_divider
+
+# proposed_dclupgrade_query=$(dcld query dclupgrade proposed-upgrade --name=$upgrade_name)
+# check_response_and_report "$proposed_dclupgrade_query" "Not Found"
+
+# test_divider
+
+# get_height current_height
+# echo "Current height is $current_height"
+
+# plan_height=$(expr $current_height + 5)
+
+# test_divider
+
+# propose=$(dcld tx dclupgrade propose-upgrade --name=$upgrade_name --upgrade-height=$plan_height --upgrade-info=$upgrade_info --from jack --yes)
+# echo "propose upgrade response: $propose"
+# check_response "$propose" "\"code\": 0"
+
+# test_divider
+
+# proposed_dclupgrade_query=$(dcld query dclupgrade proposed-upgrade --name=$upgrade_name)
+# echo "dclupgrade proposed upgrade query: $proposed_dclupgrade_query"
+# check_response_and_report "$proposed_dclupgrade_query" "\"name\": \"$upgrade_name\""
+# check_response_and_report "$proposed_dclupgrade_query" "\"height\": \"$plan_height\""
+# check_response_and_report "$proposed_dclupgrade_query" "\"info\": \"$upgrade_info\""
+
+# test_divider
+
+# approve=$(dcld tx dclupgrade approve-upgrade --name=$upgrade_name --from alice --yes)
+# echo "approve upgrade response: $approve"
+# check_response "$approve" "\"code\": 0"
+
+# test_divider
+
 upgrade_height=$(($RANDOM + 10000000))
 random_string upgrade_info
 
