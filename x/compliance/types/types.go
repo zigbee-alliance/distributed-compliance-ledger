e9e8be87095233d916fa9da7d6b3b06d30c4ff46
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-06-13 23:19:41 +0500

    Add new certifacation types for the section Compliance

--- x/compliance/types/types.go
+++ x/compliance/types/types.go
@@ -3,12 +3,19 @@ package types
 const (
 	ZigbeeCertificationType string = "zigbee"
 	MatterCertificationType string = "matter"
+	FullCertificationType   string = "Full"
+	CbSCertificationType    string = "CbS" // CbS - Certification by Similarity
+	CTPCertificationType    string = "CTP" // CTP - Certification Transfer Program
+	PFCCertificationType    string = "PFC" // PFC - Product Family Certification
 )
 
 //	List of Certification Types
 type CertificationTypes []string
 
-var CertificationTypesList = CertificationTypes{ZigbeeCertificationType, MatterCertificationType}
+var CertificationTypesList = CertificationTypes{
+	ZigbeeCertificationType, MatterCertificationType, FullCertificationType,
+	CbSCertificationType, CTPCertificationType, PFCCertificationType,
+}
 
 func IsValidCertificationType(certificationType string) bool {
 	for _, i := range CertificationTypesList {
