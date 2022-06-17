5268d5e0ab3e64b2adaf57b2975604912d98f130
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-06-17 11:10:38 +0500

    Fix golangci-lint mistakes

--- x/compliance/types/compliance_info.go
+++ x/compliance/types/compliance_info.go
@@ -28,48 +28,48 @@ func (d *ComplianceInfo) SetRevokedStatus(date string, reason string) {
 	d.Reason = reason
 }
 
-func (complianceInfo *ComplianceInfo) SetOptionalFields(msg *MsgCertifyModel) {
+func (d *ComplianceInfo) SetOptionalFields(msg *MsgCertifyModel) {
 	if msg.ProgramTypeVersion != "" {
-		complianceInfo.ProgramTypeVersion = msg.ProgramTypeVersion
+		d.ProgramTypeVersion = msg.ProgramTypeVersion
 	}
 
 	if msg.FamilyId != "" {
-		complianceInfo.FamilyId = msg.FamilyId
+		d.FamilyId = msg.FamilyId
 	}
 
 	if msg.SupportedClusters != "" {
-		complianceInfo.SupportedClusters = msg.SupportedClusters
+		d.SupportedClusters = msg.SupportedClusters
 	}
 
 	if msg.CompliantPlatformUsed != "" {
-		complianceInfo.CompliantPlatformUsed = msg.CompliantPlatformUsed
+		d.CompliantPlatformUsed = msg.CompliantPlatformUsed
 	}
 
 	if msg.CompliantPlatformVersion != "" {
-		complianceInfo.CompliantPlatformVersion = msg.CompliantPlatformVersion
+		d.CompliantPlatformVersion = msg.CompliantPlatformVersion
 	}
 
 	if msg.OSVersion != "" {
-		complianceInfo.OSVersion = msg.OSVersion
+		d.OSVersion = msg.OSVersion
 	}
 
 	if msg.CertificationRoute != "" {
-		complianceInfo.CertificationRoute = msg.CertificationRoute
+		d.CertificationRoute = msg.CertificationRoute
 	}
 
 	if msg.CertificationRoute != "" {
-		complianceInfo.CertificationRoute = msg.CertificationRoute
+		d.CertificationRoute = msg.CertificationRoute
 	}
 
 	if msg.ProgramType != "" {
-		complianceInfo.ProgramType = msg.ProgramType
+		d.ProgramType = msg.ProgramType
 	}
 
 	if msg.Transport != "" {
-		complianceInfo.Transport = msg.Transport
+		d.Transport = msg.Transport
 	}
 
 	if msg.ParentChild != "" {
-		complianceInfo.ParentChild = msg.ParentChild
+		d.ParentChild = msg.ParentChild
 	}
 }
