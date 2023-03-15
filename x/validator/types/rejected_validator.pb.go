89f32969d999518fd138b58b60c0bec5ee0717e1
DenisRybas <denis2002.rybas@gmail.com>
2023-03-15 14:52:37 +0300

    Revert "reverted some generated code"
    This reverts commit 82a19ef247bfe2f197f979fccb1080762c8ca078.

--- x/validator/types/rejected_validator.pb.go
+++ x/validator/types/rejected_validator.pb.go
@@ -6,6 +6,7 @@ package types
 import (
 	fmt "fmt"
 	_ "github.com/cosmos/cosmos-proto"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
@@ -63,11 +64,9 @@ func (m *RejectedDisableValidator) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_RejectedDisableValidator proto.InternalMessageInfo
 
-func (m *RejectedDisableValidator) GetAddress() string {
-	if m != nil {
-		return m.Address
-	}
-	return ""
+func (m *RejectedDisableValidator) GetAddress() sdk.ValAddress {
+	valAddr, _ := sdk.ValAddressFromBech32(m.Address)
+	return valAddr
 }
 
 func (m *RejectedDisableValidator) GetCreator() string {
