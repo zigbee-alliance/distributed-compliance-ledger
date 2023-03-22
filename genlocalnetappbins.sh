cd698ca1aa221ce118e48ad574a74e85effebc4a
DenisRybas <denis2002.rybas@gmail.com>
2023-03-23 02:04:14 +0300

    processed comment

--- genlocalnetappbins.sh
+++ genlocalnetappbins.sh
@@ -3,22 +3,21 @@ set -euo pipefail
 DOCKERFILE="${1:-Dockerfile-build}"
 IMAGE_TAG="${2:-dcld-build}"
 VERSION_DIR="${3:-genesis}"
-if [[ "${4}" == "v0.13.0-pre" ]]; then LATEST_VERSION="true"; else LATEST_VERSION="false"; fi
-
+MAINNET_STABLE_VERSION="${4:-""}"
 LOCALNET_DIR=".localnet"
 
 docker build -f ${DOCKERFILE} -t ${IMAGE_TAG} .
 docker container create --name ${IMAGE_TAG}-inst ${IMAGE_TAG}
 
-if ${LATEST_VERSION}; then
-    wget "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/v0.12.0/dcld"
+if [ -n "$MAINNET_STABLE_VERSION" ]; then
+    wget "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/${MAINNET_STABLE_VERSION}/dcld"
     chmod ugo+x dcld 
 fi
 
 for node_name in node0 node1 node2 node3 observer0 lightclient0; do
     if [[ -d "${LOCALNET_DIR}/${node_name}" ]]; then
         mkdir -p ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
-        if ${LATEST_VERSION}; then
+        if [ -n "$MAINNET_STABLE_VERSION" ]; then
             cp dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
         else
             docker cp ${IMAGE_TAG}-inst:/go/bin/dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
@@ -26,7 +25,7 @@ for node_name in node0 node1 node2 node3 observer0 lightclient0; do
     fi
 done
 
-if ${LATEST_VERSION}; then
+if [ -n "$MAINNET_STABLE_VERSION" ]; then
     rm dcld
 fi
 
