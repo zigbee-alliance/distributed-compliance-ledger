b695fcdbdd61b747c1d7b1b90085c1792d1cb1fb
lazizcodesatdsr <laziz.abdullaev@dsr-corporation.com>
2023-04-12 13:17:50 +0500

    fix parameter naming

--- x/compliance/types/message_update_compliance_info.go
+++ x/compliance/types/message_update_compliance_info.go
@@ -9,7 +9,7 @@ const TypeMsgUpdateComplianceInfo = "update_compliance_info"
 
 var _ sdk.Msg = &MsgUpdateComplianceInfo{}
 
-func NewMsgUpdateComplianceInfo(creator string, vid int32, pid int32, softwareVersion uint32, softwareVersionString string, certificationType string, cDVersionNumber uint32, softwareVersionCertificationStatus uint32, date string, reason string, owner string, cDCertificateID string, certificationRoute string, programType string, programTypeVersion string, compliantPlatformUsed string, compliantPlatformVersion string, transport string, familyId string, supportedClusters string, oSVersion string, parentChild string, certificationIdOfSoftwareComponent string) *MsgUpdateComplianceInfo {
+func NewMsgUpdateComplianceInfo(creator string, vid int32, pid int32, softwareVersion uint32, softwareVersionString string, certificationType string, cDVersionNumber uint32, softwareVersionCertificationStatus uint32, date string, reason string, owner string, cDCertificateID string, certificationRoute string, programType string, programTypeVersion string, compliantPlatformUsed string, compliantPlatformVersion string, transport string, familyID string, supportedClusters string, oSVersion string, parentChild string, certificationIdOfSoftwareComponent string) *MsgUpdateComplianceInfo {
 	return &MsgUpdateComplianceInfo{
 		Creator:                            creator,
 		Vid:                                vid,
@@ -29,7 +29,7 @@ func NewMsgUpdateComplianceInfo(creator string, vid int32, pid int32, softwareVe
 		CompliantPlatformUsed:              compliantPlatformUsed,
 		CompliantPlatformVersion:           compliantPlatformVersion,
 		Transport:                          transport,
-		FamilyId:                           familyId,
+		FamilyId:                           familyID,
 		SupportedClusters:                  supportedClusters,
 		OSVersion:                          oSVersion,
 		ParentChild:                        parentChild,
