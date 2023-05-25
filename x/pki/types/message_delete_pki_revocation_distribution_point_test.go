5217f8382c370ed1f2359c1260edbca147f64dd9
DenisRybas <denis.rybas@dsr-corporation.com>
2023-05-25 16:02:21 +0300

    added msg tests for delete

--- x/pki/types/message_delete_pki_revocation_distribution_point_test.go
+++ x/pki/types/message_delete_pki_revocation_distribution_point_test.go
@@ -5,11 +5,14 @@ import (
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
+	testconstants "github.com/zigbee-alliance/distributed-compliance-ledger/integration_tests/constants"
 	"github.com/zigbee-alliance/distributed-compliance-ledger/testutil/sample"
+	pkitypes "github.com/zigbee-alliance/distributed-compliance-ledger/types/pki"
+	"github.com/zigbee-alliance/distributed-compliance-ledger/utils/validator"
 )
 
 func TestMsgDeletePkiRevocationDistributionPoint_ValidateBasic(t *testing.T) {
-	tests := []struct {
+	negativeTests := []struct {
 		name string
 		msg  MsgDeletePkiRevocationDistributionPoint
 		err  error
@@ -20,20 +23,103 @@ func TestMsgDeletePkiRevocationDistributionPoint_ValidateBasic(t *testing.T) {
 				Signer: "invalid_address",
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
+		},
+		{
+			name: "empty vid",
 			msg: MsgDeletePkiRevocationDistributionPoint{
 				Signer: sample.AccAddress(),
 			},
+			err: validator.ErrFieldLowerBoundViolated,
+		},
+		{
+			name: "vid < 1",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer:             sample.AccAddress(),
+				Vid:                0,
+				Label:              "label",
+				IssuerSubjectKeyID: testconstants.SubjectKeyIDWithoutColons,
+			},
+			err: validator.ErrFieldLowerBoundViolated,
+		},
+		{
+			name: "vid > 65535",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer:             sample.AccAddress(),
+				Vid:                65536,
+				Label:              "label",
+				IssuerSubjectKeyID: testconstants.SubjectKeyIDWithoutColons,
+			},
+			err: validator.ErrFieldUpperBoundViolated,
+		},
+		{
+			name: "label empty",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer: sample.AccAddress(),
+				Vid:    1,
+			},
+			err: validator.ErrRequiredFieldMissing,
+		},
+		{
+			name: "issuerSubjectKeyId empty",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer: sample.AccAddress(),
+				Vid:    1,
+				Label:  "label",
+			},
+			err: validator.ErrRequiredFieldMissing,
+		},
+		{
+			name: "wrong IssuerSubjectKeyId format (not [0-9A-F])",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer:             sample.AccAddress(),
+				Vid:                1,
+				Label:              "label",
+				IssuerSubjectKeyID: "QWERTY",
+			},
+			err: pkitypes.ErrWrongSubjectKeyIDFormat,
+		},
+		{
+			name: "wrong IssuerSubjectKeyId format (not even number of symbols)",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer:             sample.AccAddress(),
+				Vid:                1,
+				Label:              "label",
+				IssuerSubjectKeyID: "123",
+			},
+			err: pkitypes.ErrWrongSubjectKeyIDFormat,
+		},
+		{
+			name: "wrong IssuerSubjectKeyId format (not even number of symbols)",
+			msg: MsgDeletePkiRevocationDistributionPoint{
+				Signer:             sample.AccAddress(),
+				Vid:                1,
+				Label:              "label",
+				IssuerSubjectKeyID: "123",
+			},
+			err: pkitypes.ErrWrongSubjectKeyIDFormat,
 		},
 	}
-	for _, tt := range tests {
+
+	positiveTests := []struct {
+		name string
+		msg  MsgAddPkiRevocationDistributionPoint
+	}{
+		// {
+		// 	name: "valid approve add x509cert msg",
+		// },
+	}
+
+	for _, tt := range negativeTests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.msg.ValidateBasic()
-			if tt.err != nil {
-				require.ErrorIs(t, err, tt.err)
-				return
-			}
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
+		})
+	}
+
+	for _, tt := range positiveTests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
 			require.NoError(t, err)
 		})
 	}
