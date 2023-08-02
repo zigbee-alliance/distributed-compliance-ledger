99c449af3baf4bccdd678d21a906d6f7c1db535d
Aziz Karabashov <aziz.karabashov@dsr-corporation.com>
2023-08-02 19:08:06 +0500

    Remove test that uses a non-existent certificate

--- integration_tests/upgrade/test-upgrade.sh
+++ integration_tests/upgrade/test-upgrade.sh
@@ -1053,13 +1053,7 @@ test_divider
 
 # X509 PKI
 
-echo "Assign VID to non-VID scoped root_certificate"
-result=$(echo $passphrase | $DCLD_BIN_NEW tx pki assign-vid --subject="$root_cert_subject" --subject-key-id="$root_cert_subject_key_id" --vid="$root_cert_path_new_random_vid" --from $vendor_admin_account --yes)
-check_response "$result" "\"code\": 0"
-
-test_divider
-
-echo "Assign VID to VID scoped test_root_certificate"
+echo "Assign VID to root_certificate"
 result=$(echo $passphrase | $DCLD_BIN_NEW tx pki assign-vid --subject="$test_root_cert_subject" --subject-key-id="$test_root_cert_subject_key_id" --vid="$test_root_cert_vid" --from $vendor_admin_account --yes)
 check_response "$result" "\"code\": 0"
 
