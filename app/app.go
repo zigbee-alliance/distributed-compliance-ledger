3664124a130a8233075545f90cb0c6c0a4559b7c
Alexander Shcherbakov <alexander.sherbakov@dsr-corporation.com>
2025-01-16 18:01:46 +0000

    Hot fix

--- app/app.go
+++ app/app.go
@@ -680,7 +680,7 @@ func New(
 	)
 
 	app.UpgradeKeeper.SetUpgradeHandler(
-		"v1.4",
+		"v1.4.3",
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
