56c62fb62512f5c5f00ed3b72e5ebe1fb5dc4d69
jignatova-dsr <136325382+jignatova-dsr@users.noreply.github.com>
2025-07-01 09:44:11 +0000

    Added quoits for injection and mistake protection

--- scripts/tests-after-upgrade/tests-after-upgrade.sh
+++ scripts/tests-after-upgrade/tests-after-upgrade.sh
@@ -57,7 +57,7 @@ echo "Verify that old data is not corrupted"
 # VENDORINFO
 
 echo "Verify if VendorInfo Record for VID: $vid is present or not"
-result=$(dcld query vendorinfo vendor --vid=$vid)
+result=$(dcld query vendorinfo vendor --vid="$vid")
 check_response "$result" "\"vendorID\": $vid"
 check_response "$result" "\"companyLegalName\": \"$company_legal_name\""
 check_response "$result" "\"vendorName\": \"$vendor_name\""
@@ -72,12 +72,12 @@ test_divider
 # MODEL
 
 echo "Get Model with VID: $vid PID: $pid_for_vid"
-result=$(dcld query model get-model --vid=$vid --pid=$pid_for_vid)
+result=$(dcld query model get-model --vid="$vid" --pid="$pid_for_vid")
 check_response "$result" "\"vid\": $vid"
 check_response "$result" "\"pid\": $pid_for_vid"
 
 echo "Get model version VID: $vid PID: $pid_for_vid"
-result=$(dcld query model model-version --vid=$vid --pid=$pid_for_vid --softwareVersion=$software_version)
+result=$(dcld query model model-version --vid="$vid" --pid="$pid_for_vid" --softwareVersion="$software_version")
 check_response "$result" "\"vid\": $vid"
 check_response "$result" "\"pid\": $pid_for_vid"
 check_response "$result" "\"softwareVersion\": $software_version"
@@ -88,12 +88,12 @@ check_response "$result" "\"vid\": $vid"
 check_response "$result" "\"pid\": $pid_for_vid"
 
 echo "Get Vendor Models with VID: ${vid}"
-result=$(dcld query model vendor-models --vid=$vid)
+result=$(dcld query model vendor-models --vid="$vid")
 check_response "$result" "\"pid\": $pid_for_vid"
 check_response "$result" "\"pid\": $pid_1_for_vid"
 
 echo "Get all model versions with VID: $vid PID: $pid_for_vid"
-result=$(dcld query model all-model-versions --vid=$vid --pid=$pid_for_vid)
+result=$(dcld query model all-model-versions --vid="$vid" --pid="$pid_for_vid")
 check_response "$result" "\"vid\": $vid"
 check_response "$result" "\"pid\": $pid_for_vid"
 check_response "$result" "\"softwareVersions\""
@@ -104,7 +104,7 @@ test_divider
 # COMPLIANCE
 
 echo "Get certified model vid=$vid_2 pid=$pid_for_vid_2"
-result=$(dcld query compliance certified-model --vid=$vid_2 --pid=$pid_for_vid_2 --softwareVersion=$software_version_for_vid_2 --certificationType=$certification_type)
+result=$(dcld query compliance certified-model --vid="$vid_2" --pid="$pid_for_vid_2" --softwareVersion="$software_version_for_vid_2" --certificationType="$certification_type")
 check_response "$result" "\"value\": true"
 check_response "$result" "\"vid\": $vid_2"
 check_response "$result" "\"pid\": $pid_for_vid_2"
@@ -118,7 +118,7 @@ check_response "$result" "\"pid\": $pid_for_vid_2"
 
 if [ "$ENVIRONMENT" != "mainnet" ]; then
 echo "Get revoked Model with VID: $vid_revoked PID: $pid_revoked"
-result=$(dcld query compliance revoked-model --vid=$vid_revoked --pid=$pid_revoked --softwareVersion=$software_version_revoked --certificationType=$certification_type)
+result=$(dcld query compliance revoked-model --vid="$vid_revoked" --pid="$pid_revoked" --softwareVersion="$software_version_revoked" --certificationType="$certification_type")
 check_response "$result" "\"vid\": $vid_revoked"
 check_response "$result" "\"pid\": $pid_revoked"
 check_response "$result" "\"softwareVersion\": $software_version_revoked"
@@ -131,14 +131,14 @@ check_response "$result" "\"pid\": $pid_revoked"
 fi
 
 echo "Get compliance-info model with VID: $vid_2 PID: $pid_for_vid_2"
-result=$(dcld query compliance compliance-info --vid=$vid_2 --pid=$pid_for_vid_2 --softwareVersion=$software_version_for_vid_2 --certificationType=$certification_type)
+result=$(dcld query compliance compliance-info --vid="$vid_2" --pid="$pid_for_vid_2" --softwareVersion="$software_version_for_vid_2" --certificationType="$certification_type")
 check_response "$result" "\"vid\": $vid_2"
 check_response "$result" "\"pid\": $pid_for_vid_2"
 check_response "$result" "\"softwareVersion\": $software_version_for_vid_2"
 check_response "$result" "\"certificationType\": \"$certification_type\""
 
 echo "Get device software compliance cDCertificateId=$cd_certificate_id"
-result=$(dcld query compliance device-software-compliance --cdCertificateId=$cd_certificate_id)
+result=$(dcld query compliance device-software-compliance --cdCertificateId="$cd_certificate_id")
 check_response "$result" "\"vid\": $vid_complience"
 check_response "$result" "\"pid\": $pid_complience"
 check_response "$result" "\"cDCertificateId\": \"$cd_certificate_id\""
@@ -154,7 +154,7 @@ test_divider
 # AUTH
 
 echo "Get account"
-result=$(dcld query auth account --address=$user_address)
+result=$(dcld query auth account --address="$user_address")
 check_response "$result" "\"address\": \"$user_address\""
 
 echo "Get all accounts"
@@ -165,7 +165,7 @@ check_response "$result" "\"address\": \"$user_address_1\""
 if [ "$ENVIRONMENT" != "mainnet" ]; then
 
 echo "Get rejected account"
-result=$(dcld query auth rejected-account --address=$user_rejected_address)
+result=$(dcld query auth rejected-account --address="$user_rejected_address")
 check_response "$result" "\"address\": \"$user_rejected_address\""
 
 echo "Get all rejected accounts"
@@ -183,16 +183,16 @@ result=$(dcld query pki all-certs)
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 
 echo "Get all certificates by subject (Global)"
-result=$(dcld query pki all-subject-certs --subject=$da_root_cert_subject)
+result=$(dcld query pki all-subject-certs --subject="$da_root_cert_subject")
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 
 echo "Get all certificates by SKID (Global)"
-result=$(dcld query pki cert --subject-key-id=$da_root_cert_subject_key_id)
+result=$(dcld query pki cert --subject-key-id="$da_root_cert_subject_key_id")
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 check_response "$result" "\"subjectKeyId\": \"$da_root_cert_subject_key_id\""
 
 echo "Get certificate (ALL)"
-result=$(dcld query pki cert --subject=$da_root_cert_subject --subject-key-id=$da_root_cert_subject_key_id)
+result=$(dcld query pki cert --subject="$da_root_cert_subject" --subject-key-id="$da_root_cert_subject_key_id")
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 check_response "$result" "\"subjectKeyId\": \"$da_root_cert_subject_key_id\""
 
@@ -203,7 +203,7 @@ result=$(dcld query pki all-noc-x509-certs)
 check_response "$result" "\"subject\": \"$noc_root_cert_subject\""
 
 echo "Get certificate (NOC)"
-result=$(dcld query pki noc-x509-cert --subject=$noc_root_cert_subject --subject-key-id=$noc_root_cert_subject_key_id)
+result=$(dcld query pki noc-x509-cert --subject="$noc_root_cert_subject" --subject-key-id="$noc_root_cert_subject_key_id")
 check_response "$result" "\"subject\": \"$noc_root_cert_subject\""
 check_response "$result" "\"subjectKeyId\": \"$noc_root_cert_subject_key_id\""
 
@@ -214,7 +214,7 @@ result=$(dcld query pki all-x509-certs)
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 
 echo "Get certificate (DA)"
-result=$(dcld query pki x509-cert --subject=$da_root_cert_subject --subject-key-id=$da_root_cert_subject_key_id)
+result=$(dcld query pki x509-cert --subject="$da_root_cert_subject" --subject-key-id="$da_root_cert_subject_key_id")
 check_response "$result" "\"subject\": \"$da_root_cert_subject\""
 check_response "$result" "\"subjectKeyId\": \"$da_root_cert_subject_key_id\""
 
@@ -223,7 +223,7 @@ test_divider
 # Validator Node
 
 echo "Get validator"
-result=$(dcld query validator node --address=$validator_address)
+result=$(dcld query validator node --address="$validator_address")
 check_response "$result" "\"owner\": \"$validator_address\""
 
 echo "Get all validators"
@@ -251,14 +251,14 @@ result="$(echo "$passphrase" | dcld keys add "$vendor_account" --recover)"
 
 pid_random=$(random_four_digit_int)
 echo "Add model vid=$vid_vendor pid=$pid_random"
-result=$(dcld tx model add-model --vid=$vid_vendor --pid=$pid_random --deviceTypeID=$device_type_id --productName=$product_name --from=$vendor_account --yes)
+result=$(dcld tx model add-model --vid="$vid_vendor" --pid="$pid_random" --deviceTypeID="$device_type_id" --productName="$product_name" --from="$vendor_account" --yes)
 result=$(get_txn_result "$result")
 check_response "$result" "\"code\": 0"
 
 test_divider
 
 echo "Add model version vid=$vid_vendor pid=$pid_random"
-result=$(dcld tx model add-model-version --vid=$vid_vendor --pid=$pid_random --softwareVersion=$software_version --softwareVersionString="$software_version_string" --cdVersionNumber=$cd_version_number --minApplicableSoftwareVersion=$min_applicable_software_version --maxApplicableSoftwareVersion=$max_applicable_software_version --from=$vendor_account --yes)
+result=$(dcld tx model add-model-version --vid="$vid_vendor" --pid="$pid_random" --softwareVersion="$software_version" --softwareVersionString="$software_version_string" --cdVersionNumber="$cd_version_number" --minApplicableSoftwareVersion="$min_applicable_software_version" --maxApplicableSoftwareVersion="$max_applicable_software_version" --from="$vendor_account" --yes)
 result=$(get_txn_result "$result")
 check_response "$result" "\"code\": 0"
 
@@ -267,18 +267,18 @@ test_divider
 # X509 PKI
 
 echo "Add NOC Root certificate by vendor with VID = $vendor_account"
-result=$(dcld tx pki add-noc-x509-root-cert --certificate="$noc_root_cert" --from $vendor_account --yes)
+result=$(dcld tx pki add-noc-x509-root-cert --certificate="$noc_root_cert" --from "$vendor_account" --yes)
 result=$(get_txn_result "$result")
 check_response "$result" "\"code\": 0"
 echo "Teardown: delete the added NOC Root certificate"
-result=$(dcld tx pki remove-noc-x509-root-cert --subject="$subject" --subject-key-id="$subject_key_id" --from $vendor_account --yes)
+result=$(dcld tx pki remove-noc-x509-root-cert --subject="$subject" --subject-key-id="$subject_key_id" --from "$vendor_account" --yes)
 result=$(get_txn_result "$result")
 check_response "$result" "\"code\": 0"
 
 test_divider
 
   echo "Teardown: delete keys for a $vendor_account"
-  result=$(dcld keys delete $vendor_account --yes)
+  result=$(dcld keys delete "$vendor_account" --yes)
 
 fi
 
