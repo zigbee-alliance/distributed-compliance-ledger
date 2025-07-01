c4240c217144c5d8de4a106d89652de70b3dfdf7
Andrey Kononykhin <andkononykhin@gmail.com>
2025-07-01 16:34:26 +0300

    switches ansible input generation to terraform build-in function
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- deployment/terraform/aws/outputs.tf
+++ deployment/terraform/aws/outputs.tf
@@ -12,34 +12,9 @@ output "prometheus_endpoint" {
 }
 
 output "ansible_inventory" {
-  value = {
-    all = {
-      children = {
+  value = local.ansible_inventory
+}
 
-        genesis = {
-          hosts = var.validator_config.is_genesis ? { for host in local.nodes.validator.public_ips : host => null } : null
-        }
-
-        validators = {
-          hosts = { for host in local.nodes.validator.public_ips : host => null }
-        }
-
-        private_sentries = {
-          hosts = { for host in local.nodes.private_sentries.public_ips : host => null }
-        }
-
-        public_sentries = {
-          hosts = { for host in local.nodes.public_sentries.public_ips : host => null }
-        }
-
-        seeds = {
-          hosts = { for host in local.nodes.seeds.public_ips : host => null }
-        }
-
-        observers = {
-          hosts = { for host in local.nodes.observers.public_ips : host => null }
-        }
-      }
-    }
-  }
+output "ansible_inventory_yaml" {
+  value = yamlencode(local.ansible_inventory)
 }
