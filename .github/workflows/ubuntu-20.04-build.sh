87a7a44199efb92ceefe2f72bf055647b0e8cab9
Andrey Kononykhin <andkononykhin@gmail.com>
2025-07-07 17:02:03 +0300

    fixes cosmovisor build on ubuntu 22.04
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- .github/workflows/ubuntu-20.04-build.sh
+++ .github/workflows/ubuntu-20.04-build.sh
@@ -4,30 +4,44 @@ set -eux
 
 _HOME="$HOME"
 BIN_NAME="${BIN_NAME:-dcld}"
+COSMOVISOR_BIN_NAME="${COSMOVISOR_BIN_NAME:-cosmovisor}"
+BUILD_DIR=build
+
+if [[ -z "${COSMOVISOR_VERSION:-}" ]]; then
+    >&2 echo "COSMOVISOR_VERSION is not defined"
+    exit 1
+fi
 
 GO_ROOT="$(realpath $(dirname $(which go))/..)"
 _GO_ROOT="/usr/local/go"
-_GO_BIN="$_GO_ROOT/bin"
+_GO_ROOT_BIN="$_GO_ROOT/bin"
 
 ! read -r -d '' script << EOF
     set -eux
 
-    export PATH="$_GO_BIN:\$PATH"
+    export PATH="$_GO_ROOT_BIN:\$PATH"
+    GOPATH="\${GOPATH:-\${HOME}/go}"
+    GOBIN="\${GOBIN:-\${GOPATH}/bin}"
+
     go version
+    go env GOBIN
+    go env GOPATH
 
     # install system deps
     apt-get update
     apt-get install -y software-properties-common
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:git-core/ppa
     apt-get update
-    apt-get install -y make g++ git
+    apt-get install -y make g++ git ca-certificates
     git version
 
     git config --global --add safe.directory "\$PWD"
     make build
+    "$BUILD_DIR/$BIN_NAME" version
 
-    chown "$(id -u):$(id -g)" "build/$BIN_NAME"
-    "build/$BIN_NAME" version
+    go install "cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v$COSMOVISOR_VERSION"
+    cp "\$GOPATH/bin/$COSMOVISOR_BIN_NAME" "$BUILD_DIR"
+    chown -R "$(id -u):$(id -g)" "$BUILD_DIR"
 EOF
 
 
