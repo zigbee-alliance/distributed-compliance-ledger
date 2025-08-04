489b77e05ea43ea900819883111d72069df42131
Alexander <sdushkov0@gmail.com>
2025-08-04 22:13:58 +0300

    Feature/upgrate from master branch test (#668)
    * upgrade test for master version
    
    * upgrade test for 1.5 release (#669)

--- integration_tests/run-all.sh
+++ integration_tests/run-all.sh
@@ -124,8 +124,6 @@ if [[ $TESTS_TO_RUN =~ "all" || $TESTS_TO_RUN =~ "upgrade" ]]; then
     if bash "$UPGRADE_SHELL_TEST" &>${DETAILED_OUTPUT_TARGET}; then
       rm dcld_mainnet_stable
       log "$UPGRADE_SHELL_TEST finished successfully"
-      source integration_tests/upgrade/add-new-node-after-upgrade.sh
-      check_adding_new_node
     else
       log "$UPGRADE_SHELL_TEST failed"
       exit 1
