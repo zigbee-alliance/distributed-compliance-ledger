4d15a6eb8f8898e429d35bf5346c2591b356e3b8
Alexander <sdushkov@yandex.ru>
2025-08-04 12:23:14 +0300

    edits description

--- scripts/verify_upgrade_handler.sh
+++ scripts/verify_upgrade_handler.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 VER="\"$1\""
 
 if echo "$VER" | grep 'dev"' &>/dev/null; then
-    echo "Develop release, skip upgrade handle verify"
+    echo "Develop release, skipping the upgrade handler verify"
 elif echo "$VER" | grep 'pre"' &>/dev/null; then
-    echo "Pre-release, skip upgrade handle verify"
+    echo "Pre-release, skipping the upgrade handler verify"
 elif grep 'SetUpgradeHandler(' -a1 ../app/app.go | grep "$VER" &>/dev/null; then
-    echo "Upgrade handle $VER exists"
+    echo "Upgrade handler $VER exists"
 else
-    echo "Upgrade handle not $VER exists"
+    echo "Upgrade handler $VER not exists"
     exit 1
 fi
 
