1c0ae1fdb4527a4b496152cb3bded37b5359eb60
Alexander <sdushkov0@gmail.com>
2025-08-08 22:17:10 +0300

    adding test coverage (#671)

--- genlocalnetappbins.sh
+++ genlocalnetappbins.sh
@@ -6,7 +6,12 @@ VERSION_DIR="${3:-genesis}"
 MAINNET_STABLE_VERSION="${4:-""}"
 LOCALNET_DIR=".localnet"
 
-docker build -f ${DOCKERFILE} -t ${IMAGE_TAG} .
+if env | grep GOCOVER=1; then
+    docker build --build-arg "GOCOVER=1" -f ${DOCKERFILE} -t ${IMAGE_TAG} .
+else
+    docker build --build-arg "GOCOVER=" -f ${DOCKERFILE} -t ${IMAGE_TAG} .
+fi
+
 docker container create --name ${IMAGE_TAG}-inst ${IMAGE_TAG}
 
 if [ -n "$MAINNET_STABLE_VERSION" ]; then
@@ -17,7 +22,9 @@ fi
 for node_name in node0 node1 node2 node3 observer0 lightclient0; do
     if [[ -d "${LOCALNET_DIR}/${node_name}" ]]; then
         mkdir -p ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
+        mkdir -p ${LOCALNET_DIR}/${node_name}/gocover
         if [ -n "$MAINNET_STABLE_VERSION" ]; then
+            echo "cp dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/"
             cp dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
         else
             docker cp ${IMAGE_TAG}-inst:/go/bin/dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
