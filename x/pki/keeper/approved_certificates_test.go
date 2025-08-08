88858f46463c0991d79fdbea1b345afe40a79b78
Renata Toktar <renata.toktar@dsr-corporation.com>
2025-08-08 14:16:55 +0300

    add new unit tests for pki module

--- x/pki/keeper/approved_certificates_test.go
+++ x/pki/keeper/approved_certificates_test.go
@@ -71,3 +71,143 @@ func TestApprovedCertificatesGetAll(t *testing.T) {
 		nullify.Fill(keeper.GetAllApprovedCertificates(ctx)),
 	)
 }
+
+func TestApprovedCertificatesBySubjectKeyID(t *testing.T) {
+	keeper, ctx := keepertest.PkiKeeper(t, nil)
+	
+	// Test setting and getting approved certificates by subject key ID
+	subjectKeyID := "test-key-id"
+	approvedCerts := types.ApprovedCertificatesBySubjectKeyId{
+		SubjectKeyId: subjectKeyID,
+		Certs: []*types.CertificateIdentifier{
+			{
+				Subject:      "test-subject",
+				SubjectKeyId: subjectKeyID,
+			},
+		},
+	}
+	
+	keeper.SetApprovedCertificatesBySubjectKeyID(ctx, approvedCerts)
+	
+	// Test getting the certificate
+	retrieved, found := keeper.GetApprovedCertificatesBySubjectKeyID(ctx, subjectKeyID)
+	require.True(t, found)
+	require.Equal(t, approvedCerts, retrieved)
+	
+	// Test removing the certificate
+	keeper.RemoveApprovedCertificatesBySubjectKeyID(ctx, subjectKeyID)
+	_, found = keeper.GetApprovedCertificatesBySubjectKeyID(ctx, subjectKeyID)
+	require.False(t, found)
+}
+
+func TestApprovedCertificatesBySubject(t *testing.T) {
+	keeper, ctx := keepertest.PkiKeeper(t, nil)
+	
+	// Test setting and getting approved certificates by subject
+	subject := "test-subject"
+	approvedCerts := types.ApprovedCertificatesBySubject{
+		Subject: subject,
+		Certs: []*types.CertificateIdentifier{
+			{
+				Subject:      subject,
+				SubjectKeyId: "test-key-id",
+			},
+		},
+	}
+	
+	keeper.SetApprovedCertificatesBySubject(ctx, approvedCerts)
+	
+	// Test getting the certificate
+	retrieved, found := keeper.GetApprovedCertificatesBySubject(ctx, subject)
+	require.True(t, found)
+	require.Equal(t, approvedCerts, retrieved)
+	
+	// Test removing the certificate
+	keeper.RemoveApprovedCertificatesBySubject(ctx, subject)
+	_, found = keeper.GetApprovedCertificatesBySubject(ctx, subject)
+	require.False(t, found)
+}
+
+func TestApprovedCertificates_EdgeCases(t *testing.T) {
+	keeper, ctx := keepertest.PkiKeeper(t, nil)
+	
+	// Test with empty certificates
+	emptyCert := types.ApprovedCertificates{
+		Subject:      "empty-subject",
+		SubjectKeyId: "empty-key-id",
+		Certs:        []*types.CertificateIdentifier{},
+	}
+	
+	keeper.SetApprovedCertificates(ctx, emptyCert)
+	retrieved, found := keeper.GetApprovedCertificates(ctx, emptyCert.Subject, emptyCert.SubjectKeyId)
+	require.True(t, found)
+	require.Equal(t, emptyCert, retrieved)
+	
+	// Test with nil certificates
+	nilCert := types.ApprovedCertificates{
+		Subject:      "nil-subject",
+		SubjectKeyId: "nil-key-id",
+		Certs:        nil,
+	}
+	
+	keeper.SetApprovedCertificates(ctx, nilCert)
+	retrieved, found = keeper.GetApprovedCertificates(ctx, nilCert.Subject, nilCert.SubjectKeyId)
+	require.True(t, found)
+	require.Equal(t, nilCert, retrieved)
+}
+
+func TestApprovedCertificates_NonExistent(t *testing.T) {
+	keeper, ctx := keepertest.PkiKeeper(t, nil)
+	
+	// Test getting non-existent certificates
+	_, found := keeper.GetApprovedCertificates(ctx, "non-existent", "non-existent")
+	require.False(t, found)
+	
+	_, found = keeper.GetApprovedCertificatesBySubjectKeyID(ctx, "non-existent")
+	require.False(t, found)
+	
+	_, found = keeper.GetApprovedCertificatesBySubject(ctx, "non-existent")
+	require.False(t, found)
+}
+
+func TestApprovedCertificates_Update(t *testing.T) {
+	keeper, ctx := keepertest.PkiKeeper(t, nil)
+	
+	// Create initial certificate
+	initialCert := types.ApprovedCertificates{
+		Subject:      "update-subject",
+		SubjectKeyId: "update-key-id",
+		Certs: []*types.CertificateIdentifier{
+			{
+				Subject:      "update-subject",
+				SubjectKeyId: "update-key-id",
+			},
+		},
+	}
+	
+	keeper.SetApprovedCertificates(ctx, initialCert)
+	
+	// Update the certificate
+	updatedCert := types.ApprovedCertificates{
+		Subject:      "update-subject",
+		SubjectKeyId: "update-key-id",
+		Certs: []*types.CertificateIdentifier{
+			{
+				Subject:      "update-subject",
+				SubjectKeyId: "update-key-id",
+			},
+			{
+				Subject:      "update-subject-2",
+				SubjectKeyId: "update-key-id-2",
+			},
+		},
+	}
+	
+	keeper.SetApprovedCertificates(ctx, updatedCert)
+	
+	// Verify the update
+	retrieved, found := keeper.GetApprovedCertificates(ctx, updatedCert.Subject, updatedCert.SubjectKeyId)
+	require.True(t, found)
+	require.Equal(t, updatedCert, retrieved)
+	require.Len(t, retrieved.Certs, 2)
+}
--- x/pki/keeper/msg_server_test.go
+++ x/pki/keeper/msg_server_test.go
@@ -17,3 +17,247 @@ func setupMsgServer(tb testing.TB) (types.MsgServer, context.Context) {
 
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
 }
+
+func TestMsgServer_ProposeAddX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	// Test with valid message
+	msg := &types.MsgProposeAddX509RootCert{
+		Cert:  "test-cert",
+		Signer: "cosmos1test",
+	}
+	
+	_, err := msgServer.ProposeAddX509RootCert(ctx, msg)
+	// This will likely fail due to missing setup, but we're testing the handler path
+	require.Error(t, err)
+}
+
+func TestMsgServer_ApproveAddX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgApproveAddX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.ApproveAddX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_AddX509Cert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgAddX509Cert{
+		Cert:  "test-cert",
+		Signer: "cosmos1test",
+	}
+	
+	_, err := msgServer.AddX509Cert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_ProposeRevokeX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgProposeRevokeX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.ProposeRevokeX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_ApproveRevokeX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgApproveRevokeX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.ApproveRevokeX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RevokeX509Cert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRevokeX509Cert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RevokeX509Cert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RejectAddX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRejectAddX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RejectAddX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_AddPkiRevocationDistributionPoint(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgAddPkiRevocationDistributionPoint{
+		Vid:                 1,
+		Label:               "test-label",
+		IssuerSubjectKeyID:  "test-issuer",
+		DataURL:             "https://test.com",
+		DataFileSize:        100,
+		DataDigest:          "test-digest",
+		DataDigestType:      1,
+		CrlSignerCertificate: "test-cert",
+		Signer:              "cosmos1test",
+	}
+	
+	_, err := msgServer.AddPkiRevocationDistributionPoint(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_UpdatePkiRevocationDistributionPoint(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgUpdatePkiRevocationDistributionPoint{
+		Vid:                 1,
+		Label:               "test-label",
+		IssuerSubjectKeyID:  "test-issuer",
+		DataURL:             "https://test.com",
+		DataFileSize:        100,
+		DataDigest:          "test-digest",
+		DataDigestType:      1,
+		CrlSignerCertificate: "test-cert",
+		Signer:              "cosmos1test",
+	}
+	
+	_, err := msgServer.UpdatePkiRevocationDistributionPoint(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_DeletePkiRevocationDistributionPoint(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgDeletePkiRevocationDistributionPoint{
+		Vid:                1,
+		Label:              "test-label",
+		IssuerSubjectKeyID: "test-issuer",
+		Signer:             "cosmos1test",
+	}
+	
+	_, err := msgServer.DeletePkiRevocationDistributionPoint(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_AssignVid(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgAssignVid{
+		Vid:    1,
+		Signer: "cosmos1test",
+	}
+	
+	_, err := msgServer.AssignVid(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_AddNocX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgAddNocX509RootCert{
+		Cert:  "test-cert",
+		Signer: "cosmos1test",
+	}
+	
+	_, err := msgServer.AddNocX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RemoveX509Cert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRemoveX509Cert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RemoveX509Cert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_AddNocX509IcaCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgAddNocX509IcaCert{
+		Cert:  "test-cert",
+		Signer: "cosmos1test",
+	}
+	
+	_, err := msgServer.AddNocX509IcaCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RevokeNocX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRevokeNocX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RevokeNocX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RevokeNocX509IcaCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRevokeNocX509IcaCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RevokeNocX509IcaCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RemoveNocX509IcaCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRemoveNocX509IcaCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RemoveNocX509IcaCert(ctx, msg)
+	require.Error(t, err)
+}
+
+func TestMsgServer_RemoveNocX509RootCert(t *testing.T) {
+	msgServer, ctx := setupMsgServer(t)
+	
+	msg := &types.MsgRemoveNocX509RootCert{
+		Subject:      "test-subject",
+		SubjectKeyId: "test-key-id",
+		Signer:       "cosmos1test",
+	}
+	
+	_, err := msgServer.RemoveNocX509RootCert(ctx, msg)
+	require.Error(t, err)
+}
