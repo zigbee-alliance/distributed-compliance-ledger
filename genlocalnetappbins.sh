5ae8b8808c3bea359e44efe732471b29e0f4cab5
Alexander <sdushkov0@gmail.com>
2025-08-13 14:16:22 +0300

    downloading dcld files only once, cleaning up containers (#637)

--- genlocalnetappbins.sh
+++ genlocalnetappbins.sh
@@ -14,26 +14,17 @@ fi
 
 docker container create --name ${IMAGE_TAG}-inst ${IMAGE_TAG}
 
-if [ -n "$MAINNET_STABLE_VERSION" ]; then
-    wget "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/${MAINNET_STABLE_VERSION}/dcld"
-    chmod ugo+x dcld
-fi
-
 for node_name in node0 node1 node2 node3 observer0 lightclient0; do
     if [[ -d "${LOCALNET_DIR}/${node_name}" ]]; then
         mkdir -p ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
         mkdir -p ${LOCALNET_DIR}/${node_name}/gocover
         if [ -n "$MAINNET_STABLE_VERSION" ]; then
-            echo "cp dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/"
-            cp dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
+            echo "cp ${MAINNET_STABLE_VERSION} ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/"
+            cp ${MAINNET_STABLE_VERSION} ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/dcld
         else
             docker cp ${IMAGE_TAG}-inst:/go/bin/dcld ${LOCALNET_DIR}/${node_name}/cosmovisor/${VERSION_DIR}/bin/
         fi
     fi
 done
-
-if [ -n "$MAINNET_STABLE_VERSION" ]; then
-    rm dcld
-fi
 
 docker rm ${IMAGE_TAG}-inst
