d36da7537f4de60a77adb5c745f66b94f6e843cf
Renata Toktar <renata.toktar@dsr-corporation.com>
2025-09-18 17:38:32 +0300

    fix release pipeline

--- scripts/verify_upgrade_handler.sh
+++ scripts/verify_upgrade_handler.sh
@@ -8,7 +8,7 @@ if echo "$VER" | grep 'dev"' &>/dev/null; then
     echo "Develop release, skipping the upgrade handler verify"
 elif echo "$VER" | grep 'pre"' &>/dev/null; then
     echo "Pre-release, skipping the upgrade handler verify"
-elif grep 'SetUpgradeHandler(' -a1 ../app/app.go | grep "$VER" &>/dev/null; then
+elif grep 'SetUpgradeHandler(' -a1 ./app/app.go | grep "$VER" &>/dev/null; then
     echo "Upgrade handler $VER exists"
 else
     echo "Upgrade handler $VER not exists"
