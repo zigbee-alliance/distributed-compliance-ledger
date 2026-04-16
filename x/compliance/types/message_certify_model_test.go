37efb56173a57a1c21fa549fe4c7373cafff14f9
Abdulbois <abdulbois.tursunov@dsr-corporation.com>
2026-04-16 15:16:31 +0500

    Reduce size constraint for cDCertificateId from 64 to 19
    This reverts commit cf626028722deef09106ef5a3b649f67602a3de2.
    
    Signed-off-by: Abdulbois <abdulbois.tursunov@dsr-corporation.com>

--- x/compliance/types/message_certify_model_test.go
+++ x/compliance/types/message_certify_model_test.go
@@ -239,7 +239,7 @@ func TestMsgCertifyModel_ValidateBasic(t *testing.T) {
 			err: validator.ErrFieldMaxLengthExceeded,
 		},
 		{
-			name: "CDCertificateId > 64",
+			name: "CDCertificateId > 19",
 			msg: MsgCertifyModel{
 				Signer:                sample.AccAddress(),
 				Pid:                   1,
@@ -249,7 +249,7 @@ func TestMsgCertifyModel_ValidateBasic(t *testing.T) {
 				CertificationType:     testconstants.CertificationType,
 				CDVersionNumber:       uint32(testconstants.CdVersionNumber),
 				Reason:                testconstants.Reason,
-				CDCertificateId:       tmrand.Str(65),
+				CDCertificateId:       tmrand.Str(20),
 			},
 			err: validator.ErrFieldMaxLengthExceeded,
 		},
@@ -564,7 +564,7 @@ func TestMsgCertifyModel_ValidateBasic(t *testing.T) {
 			},
 		},
 		{
-			name: "CDCertificateId >= 0 && CDCertificateId <= 64",
+			name: "CDCertificateId >= 0 && CDCertificateId <= 19",
 			msg: MsgCertifyModel{
 				Signer:                sample.AccAddress(),
 				Pid:                   1,
